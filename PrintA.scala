498aeab04408ef355cf7f605eba77edbea6a36f1
zztranger <34526892+zztranger@users.noreply.github.com>
2018-06-28 08:30:54 +0700

    6/28/2018

---
+++ PrintA.scala
@@ -0,0 +1,14 @@
+object PrintA{
+  def main(args: Array[String]): Unit = {
+    var name = List[String]("Alex", "Aloha", "Hola", "Bob")
+    for(i <- 0 until name.length)
+      {
+        if(name(i).startsWith("A"))
+          {
+            println(name(i))
+          }
+      }
+    name.foreach(c => (if(c.length()== 4) println("Yes") else println("No") ))
+    name.foreach(c => (if(c.length()> 3) println("Yes") else println("No")))
+  }
+}
